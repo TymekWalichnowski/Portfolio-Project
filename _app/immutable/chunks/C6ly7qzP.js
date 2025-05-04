c6c66fcaa876d506d85ee0d2bd8546294e38c474
TymekWalichnowski <C00301121@setu.ie>
2025-05-04 23:59:15 +0100

    Second deploy

---
+++ _app/immutable/chunks/C6ly7qzP.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_qzb9w1)==null?void 0:s.base)??"/website-project";var e;const a=((e=globalThis.__sveltekit_qzb9w1)==null?void 0:e.assets)??t;export{a,t as b};
